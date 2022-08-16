8ec69b60312c50954434fb11534a943b4ad7efc2
Lesther Bualan <lestherbualan1718@gmail.com>
2022-08-16 11:23:24 +0800

    add messenger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MessengerComponent } from './components/messenger/messenger.component';
+import { HeaderComponent } from './components/messenger/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MessengerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
